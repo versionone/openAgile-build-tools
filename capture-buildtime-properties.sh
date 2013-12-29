84ce631578e23615b7af7878c410af12a996f745
Ian Buchanan <ian.buchanan@versionone.com>
2013-12-29 09:12:48 -0500

    adding script to capture promotion properties

---
+++ capture-buildtime-properties.sh
@@ -0,0 +1,11 @@
+## Save envars corresponding to this build run for future promotion
+cat > $WORKSPACE/buildtime.properties <<EOF
+
+GIT_URL=`git config --get remote.origin.url`
+GIT_BRANCH=$GIT_BRANCH
+GIT_COMMIT=$GIT_COMMIT
+BUILDTIME_BUILD_NUMBER=$BUILD_NUMBER
+BUILDTIME_BUILD_TAG=$BUILD_TAG
+BUILDTIME_BUILD_ID=$BUILD_ID
+
+EOF
