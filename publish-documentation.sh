8a08983fa5d6069697e19984d4d8016b9fde2679
Ian Buchanan <ian.buchanan@versionone.com>
2013-12-30 10:18:24 -0500

    guard for local changes and checkout gh-pages

--- publish-documentation.sh
+++ publish-documentation.sh
@@ -4,6 +4,16 @@ set -ex
 ## e = print a trace of commands and their arguments during execution.
 ## See: http://www.gnu.org/software/bash/manual/html_node/The-Set-Builtin.html#The-Set-Builtin
 
+# ----- Prepare Branches ------------------------------------------------------
+## Stash changes so we can checkout gh-pages and a clean master.
+if ! git diff-index --quiet HEAD --; then
+  git stash
+fi
+git checkout gh-pages
+git checkout master
+
+
+
 # ----- Publish Documentation -------------------------------------------------
 ## Publishes a subdirectory "doc" of the main project to the gh-pages branch.
 ## From: http://happygiraffe.net/blog/2009/07/04/publishing-a-subdirectory-to-github-pages/
@@ -12,3 +22,8 @@ new_commit=$(echo "Auto-update docs." | git commit-tree $doc_sha -p refs/heads/g
 git update-ref refs/heads/gh-pages $new_commit
 
 
+
+# ----- Push Docs -------------------------------------------------------------
+## Push changes and pop the stack, if any.
+git push origin gh-pages
+git stash pop
