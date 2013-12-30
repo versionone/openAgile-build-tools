514c371ca76d9760ee7ceb28a09ba9977d259820
Ian Buchanan <ian.buchanan@versionone.com>
2013-12-30 10:05:16 -0500

    add common script for publishing documentation to gh-pages

---
+++ publish-documentation.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -ex
+## x = exit immediately if a pipeline returns a non-zero status.
+## e = print a trace of commands and their arguments during execution.
+## See: http://www.gnu.org/software/bash/manual/html_node/The-Set-Builtin.html#The-Set-Builtin
+
+# ----- Publish Documentation -------------------------------------------------
+## Publishes a subdirectory "doc" of the main project to the gh-pages branch.
+## From: http://happygiraffe.net/blog/2009/07/04/publishing-a-subdirectory-to-github-pages/
+doc_sha=$(git ls-tree -d HEAD doc | awk '{print $3}')
+new_commit=$(echo "Auto-update docs." | git commit-tree $doc_sha -p refs/heads/gh-pages)
+git update-ref refs/heads/gh-pages $new_commit
+
+
